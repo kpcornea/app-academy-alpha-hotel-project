b1047e6df0b7917ef3e739341f51e96e4a62d00f
Kevin Cornea <kpcornea@gmail.com>
2018-11-13 13:45:40 -0800

    part 1 completed

--- lib/room.rb
+++ lib/room.rb
@@ -1,3 +1,30 @@
 class Room
+  # attr_reader attributes
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+    @occupants.length == @capacity
+  end
+
+  def available_space
+    @capacity - @occupants.length
+  end
+
+  def add_occupant(name)
+    return false if self.full?
+    @occupants << name
+    true
+  end
 
 end
